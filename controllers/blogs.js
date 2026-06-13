92ec7285659b74409dcf87959874c1f0dc523300
JA8866 <109983530+JA8866@users.noreply.github.com>
2026-06-12 21:31:09 -0500

    finished chapter 3 exercise 7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -10,17 +10,31 @@ const blogFinder = async (req, res, next) => {
   next()
 }
 
+const errorHandler = (error, req, res, next) => {
+  console.error('error.message:', error.message)
+  console.error('error.name:', error.name)
+
+  if (
+    error.name === 'SequelizeDatabaseError' ||
+    error.name === 'SequelizeValidationError'
+  ) {
+    return res.status(400).send({ error: error.message })
+  } 
+
+  next(error)
+}
+
 router.get('/', async (req, res) => {
   const blogs = await Blog.findAll()  
   res.json(blogs)
 })
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
   try {
     const blog = await Blog.create({ ...req.body })
     res.json(blog)
   } catch (error) {
-    return res.status(400).json({ error })
+    next(error)
   }
 })
 
@@ -29,10 +43,16 @@ router.delete('/:id', blogFinder, async (req, res) => {
   res.status(204).end()
 })
 
-router.put('/:id', blogFinder, async (req, res) => {
-  req.blog.likes = req.body.likes
-  await req.blog.save()
-  res.json(req.blog)
+router.put('/:id', blogFinder, async (req, res, next) => {
+  try {
+    req.blog.likes = req.body.likes
+    await req.blog.save()
+    res.json(req.blog)
+  } catch (error) {
+    next(error)
+  }
 })
+
+router.use(errorHandler)
 
 module.exports = router
