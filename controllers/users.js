f6e6a4b5efeb3fe9e8aa86e8e2e498dcf8c6ef95
JA8866 <109983530+JA8866@users.noreply.github.com>
2026-06-16 18:25:06 -0500

    finished chapter 3 exercise 9

--- controllers/users.js
+++ controllers/users.js
@@ -2,34 +2,48 @@ const router = require('express').Router()
 
 const { User } = require('../models')
 
+const errorHandler = (error, req, res, next) => {
+  console.error('error.message:', error.message)
+  console.error('error.name:', error.name)
+
+  if (
+    error.name === 'TypeError' ||
+    error.name === 'SequelizeValidationError' ||
+    error.name === 'SequelizeUniqueConstraintError'
+  ) {
+    return res.status(400).send({ error: error.message })
+  } 
+
+  next(error)
+}
+
 router.get('/', async (req, res) => {
   const users = await User.findAll()
   res.json(users)
 })
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
   try {
     const user = await User.create(req.body)
     res.json(user)
   } catch (error) {
-    return res.status(400).json({ error })
+    next(error)
   }
 })
 
-router.put('/:username', async (req, res) => {
+router.put('/:username', async (req, res, next) => {
   try {
     const user = await User.findOne({
       where: { username: req.params.username }
     })
-    if (!user) {
-      return res.status(404).end()
-    }
     user.name = req.body.name
     await user.save()
     res.json(user)
   } catch (error) {
-    return res.status(400).json({ error })
+    next(error)
   }
 })
+
+router.use(errorHandler)
 
 module.exports = router
