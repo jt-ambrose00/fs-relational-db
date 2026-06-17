1c9673f1ac26d68a3a366d78aba2c453c9c6075d
JA8866 <109983530+JA8866@users.noreply.github.com>
2026-06-17 18:54:48 -0500

    finished chapter 3 exercise 14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -42,10 +42,15 @@ const errorHandler = (error, req, res, next) => {
 }
 
 router.get('/', async (req, res) => {
-  const where = {}
+  let where = {}
 
   if (req.query.search) {
-    where.title = { [Op.iLike]: `%${req.query.search}%` }
+    where = {
+      [Op.or]: [
+        { title: { [Op.iLike]: `%${req.query.search}%` } },
+        { author: { [Op.iLike]: `%${req.query.search}%` } }
+      ]
+    }
   }
 
   const blogs = await Blog.findAll({
